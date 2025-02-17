7e1cfe3c96333b045d8a2ba5d1058c1b92d29ebd
Emilly Ruff <novoemillyruff@.com>
2025-02-17 12:29:19 -0300

    Atualização

--- interpolacao.py
+++ interpolacao.py
@@ -1,26 +1,38 @@
-email_tmpl = """ Olá, %(nome)s
-    Tem interesse em comprar %(produto)s?
-    
-    Este porduto é ótimo para resolver %(texto)s
-    
-    Clique sgora em %(link)s
-    
-    Apenas %(quantidade)d disponíveis!
-    
-    Preço promocional %(preco).2f
-    """
-clientes = ["Maria", "João", "Bruno"]
+#!user/bin/env python
+"""Imprime a mensagem de um e-mail"""
+__version__ = "0.1.1"
 
-for cliente in clientes: 
+import sys
+import os
+
+arguments = sys.argv[1:]
+if not arguments:
+    print("informe o nome do arquivo de emails")
+    sys.exit(1)
+
+filename = arguments[0]
+templatename = arguments[1]
+
+path = os.curdir
+filepath = os.path.join(path, filename) # emails.txt
+templatepath = os.path.join(path, templatename) # email_tmpl.txt
+
+
+for line in open(filepath):
+    name, email = line.split(",")
+
+    # TODO: Susbtituir por envio de email
+    print(f"Enviando email para {email}")
+    print()
     print(
-        email_tmpl
+        open(templatepath).read()
         % {
-            "nome": cliente,
+            "name": name,
             "produto": "caneta", 
             "texto": "Ecrever muito bem", 
             "link": "https://canetaslegais.com", 
             "quantidade": 1, 
             "preco": 50.5,
-
         }
     )
+    print("-"*50)
